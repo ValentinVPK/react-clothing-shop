a429c2926ec20774894fce599481cd6bc9cd40ad
Valentin Krumov <valentinvpk@abv.bg>
2023-09-22 21:15:46 +0300

    Add and implement redux library

--- clothing-shop/src/index.js
+++ clothing-shop/src/index.js
@@ -16,11 +16,11 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         {/* <UserProvider> */}
-        <CategoriesProvider>
+        {/* <CategoriesProvider> */}
           <CartProvider>
             <App />
           </CartProvider>
-        </CategoriesProvider>
+        {/* </CategoriesProvider> */}
         {/* </UserProvider> */}
       </BrowserRouter>
     </Provider>
